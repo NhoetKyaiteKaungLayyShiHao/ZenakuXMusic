176683a51dcc3bfd48bdaf75eeaf06f9f137c786
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-07-15 17:27:19 +0630

    Update config.py

--- config.py
+++ config.py
@@ -85,14 +85,14 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/e51819c35ac9b76037fce.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e51819c35ac9b76037fce.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/91f18443ea39d9e22a075.jpg"
 
 
 def time_to_seconds(time):
