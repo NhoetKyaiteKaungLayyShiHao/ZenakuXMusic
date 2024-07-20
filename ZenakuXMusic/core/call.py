adf820105b660044181542e3c140931edebccbae
NhoetKyaiteKaungLayyShiHao <ohmyqueenmedusa15s@gmail.com>
2024-07-20 17:30:18 +0630

    Update call.py

--- ZenakuXMusic/core/call.py
+++ ZenakuXMusic/core/call.py
@@ -13,7 +13,12 @@ from pytgcalls.exceptions import (
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.input_stream.quality import (
+    MediaStream,
+    AudioQuality,
+    VideoQuality,
+    Update,
+)
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
